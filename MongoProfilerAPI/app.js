8ecd2840c4b5f7463a915f0cbeaf4c16e1ae7000
pktangyue <tangyue1004@gmail.com>
2015-07-24 18:06:45 +0800

    assgin reouter

--- MongoProfilerAPI/app.js
+++ MongoProfilerAPI/app.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var app = express();
-var router = require('./router.js');
+var router = require('./router.js').router;
 
-app.use('/api/avgtime/', router.router);
+app.use('/api/avgtime/', router);
 
 app.listen(3000, function () {
     console.log('app is listening at port 3000');
