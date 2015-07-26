4901a57a90e153c2b6ec0c6e0e185aac3d785d0b
pktangyue <tangyue1004@gmail.com>
2015-07-26 21:41:52 +0800

    add /api/ router

--- MongoProfilerAPI/app.js
+++ MongoProfilerAPI/app.js
@@ -2,6 +2,24 @@ var express = require('express');
 var app = express();
 var router = require('./router.js');
 
+app.get('/api/', function (req, res) {
+    var result = [];
+    result.push({
+        'title' : 'get avgtime by perday',
+        'href' : '/api/avgtime/:year/perday{?date,limit}'
+    });
+    result.push({
+        'title' : 'get avgtime by perweek',
+        'href' : '/api/avgtime/:year/perweek{?date,week,limit}'
+    });
+    result.push({
+        'title' : 'get avgtime by permonth',
+        'href' : '/api/avgtime/:year/permonth{?month,limit}'
+    });
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify(result, null, 4));
+});
+
 app.use('/api/avgtime/', router);
 
 module.exports = app;
